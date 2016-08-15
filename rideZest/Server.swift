cfece4fb49fd160972797c351e26cfe46aa05609
Ram Mareddy <appwise@gmail.com>
2016-08-15 12:42:45 -0700

    Created a Server class. This will contain the code for fetching JSON and images using the Uber API calls. This will contain the core networking code for OAuth and other fetching (of products, profile, rides, images, etc.)

---
+++ rideZest/Server.swift
@@ -0,0 +1,13 @@
+//
+//  Server.swift
+//  rideZest
+//
+//  Created by Ram Mareddy on 8/15/16.
+//  Copyright © 2016 infiniteZest. All rights reserved.
+//
+
+import Foundation
+
+class Server {
+    
+}
